764413303d6bcc34174994eaae69781eb502391c
athishulleri01 <athishulleri@gmail.com>
2025-08-21 16:23:32 +0530

    chore: initialize react project and installed packages

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <h1 class="text-3xl font-bold underline text-amber-300">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
